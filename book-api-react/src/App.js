b4baed983e3f3ed2b814065457f7a4e52f06a0a6
AlisherAbdykhamitov <note12312@mail.ru>
2023-01-28 19:05:57 +0300

    first my practice with react

---
+++ book-api-react/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class='firstHi'>
+      <h2>Hello Alisher</h2>
+    </div>
+  );
+}
+
+export default App;
